d4ec450a9f0e3a0f2b4b7bbc248f9b041209ff99
Hanna Tubolets <anna.tubolets@gmail.com>
2023-04-05 18:38:42 +0300

    Deleted unnecessary folder

--- src/serveses/Api.jsx
+++ src/serveses/Api.jsx
@@ -3,15 +3,22 @@ import axios from 'axios';
 const BASE_URL = 'https://rickandmortyapi.com/api';
 
 export async function getAllCharacters() {
-  const response = await axios.get(`${BASE_URL}/character`);
+  const response = await axios.get(
+    `${BASE_URL}/character`
+    // env variables
+    // `${process.env.REACT_APP__API_DOMAIN}/character`
+  );
   // console.log(response.data.results); // add this line
   return response.data.results;
 }
 
 export async function getCharacterDetails(id) {
-  const response = await axios.get(`${BASE_URL}/character/${id}`);
+  const response = await axios.get(
+    `${BASE_URL}/character/${id}`
+    // `${process.env.REACT_APP__API_DOMAIN}/character//${id}`
+  );
   // console.log(response.data); // add this line to check the data
   return response.data;
 }
 
-getCharacterDetails(1).then(data => console.log(data));
+// getCharacterDetails(1).then(data => console.log(data));
